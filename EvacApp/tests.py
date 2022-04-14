4fb1d7bb4fdce4df68556d0c484a663804696178
Samuel Hall <shall5@umbc.edu>
2022-04-14 00:21:22 -0400

    Added another test for register form

--- EvacApp/tests.py
+++ EvacApp/tests.py
@@ -15,4 +15,15 @@ class TestCustomUserCreationForm(TestCase):
         self.assertIn("password1", form.fields)
         self.assertIn("password2", form.fields)
 
-
+    # Tests that the form saves the username and password
+    def test_form_saves_user_info(self):
+        user = User.objects.create_user(
+            username='username',
+            password='password123'
+        )
+        userForm = CustomUserCreationForm(
+            data={'username': 'username', 'password1': 'password', 'password2': 'password'})
+        if userForm.is_valid():
+            user1 = userForm.save()
+            self.assertEquals(user1.username, user.username)
+            self.assertEquals(user1.password1, user.password)
