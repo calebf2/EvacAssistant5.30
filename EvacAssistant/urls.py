db3304629e16ccfc3fd4aca791d55bfbc46688b4
Sam-NW <cnwangw1@umbc.edu>
2022-05-12 10:01:22 -0400

    Database

--- EvacAssistant/urls.py
+++ EvacAssistant/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('EvacApp.urls')),
-    path('register', include('EvacApp.urls'))
-    path('', include('main.urls', namespace="main"))
+    path('register', include('EvacApp.urls')),
+    #path('', include('main.urls', namespace="main"))
+
 ]
 
 #if settings.DEBUG:
