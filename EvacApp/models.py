dc9a9f1e552bbb4bd7d89f7f99d9a8d218364667
calebf2 <calebf2@umbc.edu>
2022-05-12 15:18:32 -0400

    Trying to implement the Google Map

--- EvacApp/models.py
+++ EvacApp/models.py
@@ -13,3 +13,8 @@ class AddressProfile(models.Model):
     country = models.CharField(verbose_name='Country', max_length=40, null=True, blank=True)
     longitude = models.CharField(verbose_name='Longitude', max_length=50, null=True, blank=True)
     latitude = models.CharField(verbose_name='Latitude', max_length=50, null=True, blank=True)
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=500)
+    password = models.CharField(max_length=500)
